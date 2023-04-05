c3c9316d0dcf8b5196d89a4aff6e20c274f42f70
Sachin Kanodia <sackanod@WKMZT733A04F.global.publicisgroupe.net>
2023-04-05 21:05:10 +0530

    Add theme and text style

--- core-design/src/main/java/com/sachin/core_design/theme/Theme.kt
+++ core-design/src/main/java/com/sachin/core_design/theme/Theme.kt
@@ -1,30 +1,53 @@
 package com.sachin.core_design.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val DarkColorScheme = darkColorScheme(
+    primary = Brand70,
+    onPrimary = Neutral10,
+    primaryContainer = Neutral10,
+    onPrimaryContainer = Neutral10,
+    secondary = Success70,
+    onSecondary = Neutral10,
+    secondaryContainer = Success70,
+    onSecondaryContainer = Neutral10,
+    tertiary = Warning70,
+    onTertiary = Neutral10,
+    tertiaryContainer = Warning70,
+    onTertiaryContainer = Neutral10,
+    background = Neutral10,
+    onBackground = Neutral100,
+    surface = Neutral10,
+    onSurface = Neutral100,
+    error = Error70,
+    errorContainer = Error70,
+    onErrorContainer = Neutral10,
+    outline = Brand70,
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val LightColorScheme = lightColorScheme(
+    primary = Brand50,
+    onPrimary = Neutral100,
+    primaryContainer = Brand30,
+    onPrimaryContainer = Neutral100,
+    secondary = Success50,
+    onSecondary = Neutral10,
+    secondaryContainer = Success50,
+    onSecondaryContainer = Neutral10,
+    tertiary = Warning50,
+    onTertiary = Neutral10,
+    tertiaryContainer = Warning50,
+    onTertiaryContainer = Neutral10,
+    background = Neutral100,
+    onBackground = Neutral10,
+    surface = Neutral100,
+    onSurface = Neutral10,
+    error = Error50,
+    errorContainer = Error50,
+    onErrorContainer = Neutral100,
+    outline = Brand50,
 )
 
 @Composable
@@ -32,16 +55,28 @@ fun PopulationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
-
     MaterialTheme(
-        colors = colors,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content
     )
+}
+
+object PopulationTheme {
+    val colorScheme: ColorScheme
+        @Composable
+        get() = MaterialTheme.colorScheme
+
+    val typography: Typography
+        @Composable
+        get() = MaterialTheme.typography
+
+    val shapes: Shapes
+        @Composable
+        get() = MaterialTheme.shapes
 }
--- core-design/src/main/java/com/sachin/core_design/theme/Type.kt
+++ core-design/src/main/java/com/sachin/core_design/theme/Type.kt
@@ -1,6 +1,6 @@
 package com.sachin.core_design.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -8,21 +8,109 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = 0.sp,
         fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    displayMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp,
+        fontWeight = FontWeight.W500,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp,
+        fontWeight = FontWeight.W400,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+        fontWeight = FontWeight.W400,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp,
+        fontWeight = FontWeight.W400,
+    ),
 )
